901d610d1cb98090ff55cd79715f96046ed05346
Zackrey Bichsel <biksel09@gmail.com>
2024-09-15 10:01:01 -0500

    added Employee.cs, updated Program.cs to allow for larger data collection of employee names

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+namespace CatWorx.BadgeMaker
+{
+    class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public int Id;
+        public string PhotoUrl;
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public string GetFullName() {
+            return FirstName + " " + LastName;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static List<string> GetEmployees()
+        static List<Employee> GetEmployees()
         {
-            List<string> employees = new List<string>();
+            List<Employee> employees = new List<Employee>();
             while (true)
             {
                 Console.WriteLine("Please enter a name: (leave empty to exit)");
@@ -16,20 +16,21 @@ namespace CatWorx.BadgeMaker
                 {
                     break;
                 }
-                employees.Add(input);
+                Employee currentEmployee = new Employee(input, "Smith");
+                employees.Add(currentEmployee);
             }
             return employees;
         }
-        static void PrintEmployees(List<string> employees)
+        static void PrintEmployees(List<Employee> employees)
         {
             for (int i = 0; i < employees.Count; i++)
             {
-                Console.WriteLine(employees[i]);
+                Console.WriteLine(employees[i].GetFullName());
             }
         }
         static void Main(string[] args)
         {
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
             PrintEmployees(employees);
         }
     }
